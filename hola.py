94ec09ed0a1467e1ca250a30d9054f4a2917bf80
doplax <doplax@DESKTOP-418UNQM.localdomain>
2022-11-20 23:35:25 +0100

    build: añadida la página de inicio y el menu-desktop

---
+++ hola.py
@@ -0,0 +1,30 @@
+
+edad = int(input("Cual es tu edad?"))
+if (edad <18):
+    print("No puedes pasar")
+else:
+    genero = input("Cual es tu sexo?")
+
+    if (genero == "hombre"):
+        print("Ehoraburan, acabas de entrar al patriarcado") 
+    if (genero == "mujer"):
+        print("Vete, Zorra!!")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
